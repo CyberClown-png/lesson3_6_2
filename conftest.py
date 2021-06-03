13cb3d9fb95af783792797e8ba976401c957cf1e
CyberClown-png <bkolesnsk0@gmail.com>
2021-06-03 19:48:40 +0300

    editet test_items.py adding

--- conftest.py
+++ conftest.py
@@ -1,10 +1,17 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: es or ru etc")
+
+
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    language = request.config.getoption("language")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207//"
 
+
+def test_guest_should_see_button_add(browser):
+    browser.get(f"http://selenium1py.pythonanywhere.com/{browser.language}/catalogue/coders-at-work_207//")
+    browser.implicitly_wait(5)
+    button = browser.find_element_by_class_name("btn-add-to-basket")
